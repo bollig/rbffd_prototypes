dbc63aa9fab85b97bda50c8ec3ab0ecd520bdef1
Evan Bollig <bollig@scs.fsu.edu>
2011-07-14 09:42:53 -0600

    Added the vortex rollup with diffusion exact solutions in mathematica and a test code in matlab. I will soon be ready to implement in C++

---
+++ matlab/vortex_roll_with_diffusion/Calc_Lsfc_fd.m
@@ -0,0 +1,68 @@
+clear all
+
+% Add diffusion timestepping in matlab
+% Implement in C++
+% 
+USE_KDTREE = 0;
+
+nodes = load('~/GRIDS/md/md400.dat'); nodes = nodes(:,1:3);
+N = length(nodes);
+dim = size(nodes, 2);
+fdsize = 33;
+ep = 2;
+
+% Use with distance table
+[dist,idx] = sort(sqrt(max(0,2*(1-nodes(:,1)*nodes(:,1)'-nodes(:,2)*nodes(:,2)'-nodes(:,3)*nodes(:,3)'))));
+
+rbf   = @(ep,rd) exp(-(ep*rd).^2);
+drbf  = @(ep,rd) -2*ep^2*exp(-(ep*rd).^2);
+d2rbf = @(ep,rd) -2*ep^2*exp(-(ep*rd).^2) + 4*ep^4*rd.^2.*exp(-(ep*rd).^2);
+
+weightsLsfc = zeros(N*fdsize,1);
+ind_i = zeros(N*fdsize,1);
+ind_j = zeros(N*fdsize,1);
+
+A = ones(fdsize+1,fdsize+1); A(end,end) = 0;
+B = zeros(fdsize+1,1);
+if USE_KDTREE
+    root = kdtree_build(nodes);
+end 
+
+for j=1:N
+ 
+    if USE_KDTREE
+        % Use KDTREE
+        idx = kdtree_k_nearest_neighbors(root, nodes(j,:), fdsize);
+        idx = idx(fdsize:-1:1);
+        % Euclidean distances
+        d2 = 0;
+        for i = 1:dim
+           d2 = d2 + (nodes(idx,i) - nodes(j,i)).^2;
+          % d2 = d2 + (nodes(:,i) - nodes(j,i)).^2;
+        end
+        dist = sqrt(d2);
+        
+        imat = idx(1:fdsize);
+        ind_i((j-1)*fdsize+1:j*fdsize) = j;
+        ind_j((j-1)*fdsize+1:j*fdsize) = imat;
+        rd = sqrt(max(0,2*(1-nodes(imat,1)*nodes(imat,1).'-nodes(imat,2)*nodes(imat,2).'-nodes(imat,3)*nodes(imat,3).')));
+    else 
+    %   Use with distance table    
+     ind_i((j-1)*fdsize+1:j*fdsize) = j;
+     ind_j((j-1)*fdsize+1:j*fdsize) = idx(1:fdsize,j); 
+     idm = idx(1:fdsize,j);
+     rd = sqrt(max(0,2*(1-nodes(idm,1)*nodes(idm,1).'-nodes(idm,2)*nodes(idm,2).'-nodes(idm,3)*nodes(idm,3).')));
+    end
+    rdv = rd(:,1);
+   
+    A(1:fdsize,1:fdsize) = rbf(ep,rd);
+    [LA,UA,P] = lu(A);
+
+    B(1:fdsize) = 1/4*( (4-rdv.^2).*d2rbf(ep,rdv) + (4 - 3*rdv.^2).*drbf(ep,rdv) );  %weights L_sfc
+    weights = UA\(LA\(P*B));
+    weightsLsfc((j-1)*fdsize+1:j*fdsize) = weights(1:fdsize);
+    
+end
+
+Lsfc = sparse(ind_i,ind_j,weightsLsfc,N,N); 
+plot(real(eig(full(Lsfc))), imag(eig(full(Lsfc))),'.')
