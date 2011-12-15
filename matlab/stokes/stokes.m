169edb20483962f5287666626e44107dc12e847b
Evan Bollig <bollig@gmail.com>
2011-12-15 01:53:18 -0700

    Full system is formed. Need to detect contant eta. Also wonder if there is a way to form a conservative RBFFD stencil

--- matlab/stokes/stokes.m
+++ matlab/stokes/stokes.m
@@ -6,21 +6,32 @@ function [LHS] = stokes(nodes, N, n, useHV)
 global RBFFD_WEIGHTS; 
 
 N = length(nodes);
-% Allocate matrix with 4 blocks of NxN and a total of 
-% 9*N*n nonzeros (9 nonzero blocks with N*n nonzeros each)
-% With row of 1's
-%L = spalloc(4*N+1, 4*N, 9*n*N+(4*N)); 
-LHS = spalloc(4*N+4, 4*N+4, 9*n*N+2*(4*N)+2*(3*N));
+% Allocate matrix with 4 blocks of NxN 
+% Number of non-zeros: up to 15 blocks fill with RBFFD weights, plus 3*2*N
+% for constraints
+LHS = spalloc(4*N+4, 4*N+4, 15*n*N + 6*N);
 % Wthout row of 1's
 %L = spalloc(4*N, 4*N, 9*n*N); 
 
-eta = 1;
+eta = ones(N,1);
+
+dEta_dx = RBFFD_WEIGHTS.xsfc * eta; 
+dEta_dy = RBFFD_WEIGHTS.ysfc * eta; 
+dEta_dz = RBFFD_WEIGHTS.zsfc * eta; 
 
 %% %%%%%%  Column 1 %%%%%%%%%%%%
 
 diag_row_ind = (1:N) + 0*N;
 diag_col_ind = (1:N) + 0*N;
-LHS(diag_row_ind, diag_col_ind) = -eta * RBFFD_WEIGHTS.lsfc; 
+LHS(diag_row_ind, diag_col_ind) = -(2 * diag(dEta_dx) * RBFFD_WEIGHTS.xsfc + diag(dEta_dy) * RBFFD_WEIGHTS.ysfc + diag(dEta_dz) * RBFFD_WEIGHTS.zsfc) - diag(eta) * RBFFD_WEIGHTS.lsfc; 
+
+diag_row_ind = (1:N) + 1*N;
+diag_col_ind = (1:N) + 0*N;
+LHS(diag_row_ind, diag_col_ind) = -diag(dEta_dx) * RBFFD_WEIGHTS.ysfc; 
+
+diag_row_ind = (1:N) + 2*N;
+diag_col_ind = (1:N) + 0*N;
+LHS(diag_row_ind, diag_col_ind) = -diag(dEta_dx) * RBFFD_WEIGHTS.zsfc; 
 
 diag_row_ind = (1:N) + 3*N;
 diag_col_ind = (1:N) + 0*N;
@@ -29,9 +40,17 @@ LHS(diag_row_ind, diag_col_ind) = RBFFD_WEIGHTS.xsfc;
 
 %% %%%%%%  Column 2 %%%%%%%%%%%%
 
+diag_row_ind = (1:N) + 0*N;
+diag_col_ind = (1:N) + 1*N;
+LHS(diag_row_ind, diag_col_ind) = -diag(dEta_dy) * RBFFD_WEIGHTS.xsfc; 
+
 diag_row_ind = (1:N) + 1*N;
 diag_col_ind = (1:N) + 1*N;
-LHS(diag_row_ind, diag_col_ind) = -eta * RBFFD_WEIGHTS.lsfc; 
+LHS(diag_row_ind, diag_col_ind) = -(diag(dEta_dx) * RBFFD_WEIGHTS.xsfc + 2 * diag(dEta_dy) * RBFFD_WEIGHTS.ysfc + diag(dEta_dz) * RBFFD_WEIGHTS.zsfc) - diag(eta) * RBFFD_WEIGHTS.lsfc; 
+
+diag_row_ind = (1:N) + 2*N;
+diag_col_ind = (1:N) + 1*N;
+LHS(diag_row_ind, diag_col_ind) = -diag(dEta_dy) * RBFFD_WEIGHTS.zsfc; 
 
 diag_row_ind = (1:N) + 3*N;
 diag_col_ind = (1:N) + 1*N;
@@ -39,9 +58,18 @@ LHS(diag_row_ind, diag_col_ind) = RBFFD_WEIGHTS.ysfc;
 
 %% %%%%%%  Column 3 %%%%%%%%%%%%
 
+
+diag_row_ind = (1:N) + 0*N;
+diag_col_ind = (1:N) + 2*N;
+LHS(diag_row_ind, diag_col_ind) = -diag(dEta_dz) * RBFFD_WEIGHTS.xsfc; 
+
+diag_row_ind = (1:N) + 1*N;
+diag_col_ind = (1:N) + 2*N;
+LHS(diag_row_ind, diag_col_ind) = -diag(dEta_dz) * RBFFD_WEIGHTS.ysfc; 
+
 diag_row_ind = (1:N) + 2*N;
 diag_col_ind = (1:N) + 2*N;
-LHS(diag_row_ind, diag_col_ind) = -eta * RBFFD_WEIGHTS.lsfc; 
+LHS(diag_row_ind, diag_col_ind) = -(diag(dEta_dx) * RBFFD_WEIGHTS.xsfc + diag(dEta_dy) * RBFFD_WEIGHTS.ysfc + 2 * diag(dEta_dz) * RBFFD_WEIGHTS.zsfc) - diag(eta) * RBFFD_WEIGHTS.lsfc; 
 
 diag_row_ind = (1:N) + 3*N;
 diag_col_ind = (1:N) + 2*N;
@@ -65,24 +93,31 @@ LHS(diag_row_ind, diag_col_ind) = RBFFD_WEIGHTS.zsfc;
 
 %%%%%%%%%%%% SYSTEM IS SINGULAR, ADD CONST TO CONSTRAIN IT %%%%%%%
 
-% diag_row_ind = (1:1) + 4*N;
-% diag_col_ind = (1:4*N) + 0*N;
-% LHS(diag_row_ind, diag_col_ind) = 1; 
+% [Usvd, Ssvd, Vsvd, flagSVD] = svds(LHS,20,0);
+% sing_value_indices_before = find(max(Ssvd) < 1e-5)
+
+diag_row_ind = (1:1) + 4*N;
+diag_col_ind = (1:4*N) + 0*N;
+LHS(diag_row_ind, diag_col_ind) = 1; 
 % 
-ind = (1:N)+3*N;
-LHS(4*N+1, ind) = 1; 
-LHS(ind, 4*N+1) = 1; 
 ind = (1:N)+0*N; 
 LHS(ind, 4*N+2) = 1; 
 LHS(4*N+2, ind) = 1; 
+
 ind = (1:N)+1*N; 
 LHS(ind, 4*N+3) = 1; 
 LHS(4*N+3, ind) = 1; 
+
 ind = (1:N)+2*N; 
 LHS(ind, 4*N+4) = 1; 
 LHS(4*N+4, ind) = 1; 
 
-[Usvd, Ssvd, Vsvd, flagSVD] = svds(LHS,100,0);
-sing_value_indices = find(max(Ssvd) < 1e-6)
+ind = (1:N)+3*N;
+LHS(4*N+1, ind) = 1; 
+LHS(ind, 4*N+1) = 1; 
+% 
+% 
+% [Usvd, Ssvd, Vsvd, flagSVD] = svds(LHS,100,0);
+% sing_value_indices = find(max(Ssvd) < 1e-6)
 
 end
