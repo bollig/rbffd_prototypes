70a8a73070c7d22731b795b175f27d689ce53ac5
Evan Bollig <bollig@gmail.com>
2012-06-05 12:06:26 -0400

    Regenerating figures because the labels were too small in the paper

---
+++ matlab/cosine_bell/plot_eigenvalues.m
@@ -0,0 +1,18 @@
+function [] = plot_eigenvalues(filename, mytitle)
+
+figure
+set(gcf,'Position',[100 100 720 650])
+evals = dlmread(filename);
+plot(real(evals), imag(evals), 'o','LineWidth',1,'MarkerEdgeColor','b','MarkerFaceColor','g','MarkerSize',6);
+axis tight;
+grid on;
+if nargin < 2
+%    title(filename,'Interpreter', 'None', 'FontSize',20);
+else 
+    title(mytitle,'Interpreter', 'Latex', 'FontSize',20);
+end
+ylabel('Im $\lambda$','Interpreter', 'LaTex','FontSize', 28);
+xlabel('Re $\lambda$','Interpreter', 'LaTex','FontSize', 28);
+set(gca,'FontSize',22)
+hold off
+end
